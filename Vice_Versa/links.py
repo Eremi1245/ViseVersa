c33f936271e3bbf797078269130ae2fb16153d6d
Alex <xxxmessixxx@mail.ru>
2020-10-04 13:43:40 +0300

    add reverse future

--- Vice_Versa/links.py
+++ Vice_Versa/links.py
@@ -4,5 +4,9 @@ def home(request):
 	return render(request,'home.html')
 
 def reverse(request):
-	return render(request,'reverse.html')
+	text=request.GET['text_zone']
+	text_words=len(text.split(' '))
+	text_reverse=text[::-1]
+	return render(request,'reverse.html',{'text_reverse':text_reverse,
+		'text_words':text_words,'text':text})
 
